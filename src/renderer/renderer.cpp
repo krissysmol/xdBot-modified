d6e80bed858228727ba788d3ddba9a6eb4b834f4
Krissy! <krissumisu.blueberry.07@gmail.com>
2025-12-30 09:11:39 +0800

    update src/renderer/renderer.cpp

--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -84,6 +84,7 @@ class $modify(FMODAudioEngine) {
 
         if (path != "playSound_01.ogg" || !Global::get().renderer.recordingAudio)
             return FMODAudioEngine::playEffect(path, speed, p2, volume);
+        return 0
     }
 
 };
